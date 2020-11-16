3a5d1689d80a8fe8df1c0ad4b8a65130207b8415
Jerry <jerrydaexplorer@gmail.com>
2020-11-17 00:56:22 +0100

    read more, show less conditional rendering

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name}/>
@@ -9,7 +10,12 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price"><small>starting at N{price}</small></h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info: `${info.substring(0,150)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? ' << show less' : ' >> read more'}
+          </button>
+        </p>
         <button className="delete-btn">Not interested</button>
       </footer>
     </article>
